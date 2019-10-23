c85ab46c95f854cebc0eb8eff7b8863d81b8fe8f
rebinti <rebin.shaju@titechglobal.in>
2019-10-23 06:04:08 +0000

    CONFLICT FIX - deleted the Vuetique themplate folder from rcgvue(1)

--- src/themes/vuetique/components/core/blocks/Microcart/ApplyButton.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <button
-    class="border-0 bg-grey-dark text-xs text-white uppercase font-medium py-1 px-2 inline-flex"
-    data-testid="applyButton"
-  >
-    {{ $t('Apply') }}
-  </button>
-</template>
