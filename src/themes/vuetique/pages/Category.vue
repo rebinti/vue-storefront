4026e790fca10face6224d0523f07ec84973c39b
rebinti <rebin.shaju@titechnologies.org>
2019-12-18 13:42:43 +0530

    testing the filter behind scroll issue(1)

--- src/themes/vuetique/pages/Category.vue
+++ src/themes/vuetique/pages/Category.vue
@@ -251,8 +251,16 @@ export default {
   .mobile_filter .sidebar .Accordion{
     background: #000;
   }
-  .openfilter{
+  body.openfilter{
     overflow-y: hidden;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    height: 100%;
+    max-height: 100vh;
+    overflow: hidden;
+    padding-bottom: 1px;
   } 
   .b_crumb{ display: none; }
 
