284ea078c69801d4ff83e1cc78593876fa9402c6
unknown <lijo.john@rcggs.com>
2020-05-04 20:56:11 +0530

    prismic cms- bug

--- src/modules/dnd-prismic-cms/store/mutations.ts
+++ src/modules/dnd-prismic-cms/store/mutations.ts
@@ -3,7 +3,8 @@ import * as types from './mutation-types'
 
 export const mutations: MutationTree<any> = {
   [types.SET_PRISMIC_CMS_BLOCKS] (state, blocks) {
-    state.prismicCmsBlocks = blocks
+    // state.prismicCmsBlocks = blocks
+    state.prismicCmsBlocks.push(...blocks)
   },
   [types.SET_PRISMIC_CMS_PAGES] (state, pages) {
     state.prismicCmsPages = pages
