8951143a7d2bd0d265cb90fb17ac592bd7d26bc5
rebinti <rebin.shaju@titechnologies.org>
2020-07-08 18:27:13 +0530

    Removed the default category value from the breadcrumb

--- src/themes/vuetique/components/core/Breadcrumbs.vue
+++ src/themes/vuetique/components/core/Breadcrumbs.vue
@@ -2,9 +2,10 @@
   <div class="bg-grey-lightest">
     <div class="container text-xs text-grey leading-normal truncate py-4 breadcrumbs">
       <span v-for="link in routes" :key="link.route_link">
-        <router-link :to="localizedRoute(link.route_link)" class="text-grey">
+
+        <router-link :to="localizedRoute(link.route_link)" v-if='(link.name!="Default Category")' class="text-grey">
           {{ link.name | htmlDecode }}
-        </router-link> <span class="px-2">&rsaquo;</span>
+        </router-link> <span v-if='(link.name!="Default Category")' class="px-2">&rsaquo;</span>
       </span>
       <span class="text-grey-dark">
         {{ activeRoute | htmlDecode }}
