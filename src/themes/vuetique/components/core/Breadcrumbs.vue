3ffa3dba5e5c86a221f568e00af8482bd5b1cea4
unknown <lijo.john@rcggs.com>
2020-07-09 10:16:52 +0530

    Adding Home redirct in breadcrumbs

--- src/themes/vuetique/components/core/Breadcrumbs.vue
+++ src/themes/vuetique/components/core/Breadcrumbs.vue
@@ -2,10 +2,13 @@
   <div class="bg-grey-lightest">
     <div class="container text-xs text-grey leading-normal truncate py-4 breadcrumbs">
       <span v-for="link in routes" :key="link.route_link">
-
-        <router-link :to="localizedRoute(link.route_link)" v-if='(link.name!="Default Category")' class="text-grey">
+        <router-link :to="localizedRoute('/')" v-if='(link.name =="Default Category")' class="text-grey">
+         Home
+        </router-link> 
+         <router-link :to="localizedRoute(link.route_link)" v-else class="text-grey">
           {{ link.name | htmlDecode }}
-        </router-link> <span v-if='(link.name!="Default Category")' class="px-2">&rsaquo;</span>
+        </router-link> 
+        <span class="px-2">&rsaquo;</span>
       </span>
       <span class="text-grey-dark">
         {{ activeRoute | htmlDecode }}
