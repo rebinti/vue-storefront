ce0e050fd485b86faaa1e827f27ddcaa590de943
rebinti <rebin.shaju@titechnologies.org>
2020-12-15 13:57:45 +0530

    Updated the Error and PageNotFound page - slider and content

--- src/themes/vuetique/pages/Error.vue
+++ src/themes/vuetique/pages/Error.vue
@@ -39,7 +39,7 @@
                       :key="product.id"
                        v-if="!product._dontShowInListingFlag"
                     >
-                      <product-tile
+                      <product-tile-carousel
                         class="collection-product"
                         :product="product"
                         :labels-active="false"
@@ -64,17 +64,20 @@ import { prepareQuery } from '@vue-storefront/core/modules/catalog/queries/commo
 import { Carousel, Slide } from 'vue-carousel'
 import NoSSR from 'vue-no-ssr'
 
-import ProductTile from 'theme/components/core/ProductTile.vue'
+import ProductTileCarousel from 'theme/components/core/ProductTileCarousel'
 
 export default {
   name: 'Error',
   data () {
     return {
       config: {
-        perPageCustom: [[576, 2], [1024, 4]],
-        paginationEnabled: true,
+        perPageCustom: [[0, 2], [768, 3], [1024, 4],[1440, 6],[1599, 5], [1600, 6], [1920, 7],[2560, 8]],
+        paginationEnabled: false,
         loop: false,
-        paginationSize: 6
+        paginationSize: 5,
+        navigationEnabled: true,
+        navigationNextLabel: `<button type="button" class="carousel-nav-nxt"><svg width="1em" height="1em" fill="currentColor" viewBox="0 0 24 22"><path d="M.75 11h22.5m-10.5 10.5L23.25 11 12.75.5" stroke="#0C1214" fill="none" stroke-linecap="round" stroke-linejoin="round"></path></svg></button>`,
+        navigationPrevLabel: `<button type="button" class="carousel-nav-pre"><svg width="1em" height="1em" fill="currentColor" viewBox="0 0 24 22"><path d="M23.25 11H.75M11.25.5L.75 11l10.5 10.5" stroke="#0C1214" fill="none" stroke-linecap="round" stroke-linejoin="round"></path></svg></button>`        
       }
     }
   },
@@ -86,7 +89,7 @@ export default {
   components: {
     Slide,
     Carousel,
-    ProductTile,
+    ProductTileCarousel,
     'no-ssr': NoSSR
   },
   methods: {
@@ -134,4 +137,69 @@ export default {
       @apply text-primary;
     }
   }
+ .product {
+    &.collection-product {
+      padding: 0;
+    }
+  }
+
+.collection-product {
+  .product-link {
+    @apply px-sm;
+  }
+
+  .product-image {
+    height: auto;
+    will-change: opacity;
+
+    img {
+      max-width: 100%;
+      max-height: 100%;
+      height: auto;
+      vertical-align: bottom;
+    }
+  }
+}
+
+.VueCarousel {
+  .VueCarousel-dot {
+    line-height: 1;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  .VueCarousel-navigation--disabled {
+    display: none;
+  }
+
+
+  .carousel-nav-nxt { 
+    position: absolute;
+    left: -75px;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background: #ffffff;
+    bottom: 15px;
+    border: 1px solid #e2dddd;
+  }
+  .carousel-nav-nxt svg {
+    margin: 0 auto;
+  }
+  .carousel-nav-pre svg {
+    margin: 0 auto;
+  }
+  .carousel-nav-pre {
+    position: absolute;
+    right: -75px;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background: #ffffff;
+    bottom: 15px;
+    border: 1px solid #e2dddd;
+  }
+}  
 </style>
--- src/themes/vuetique/pages/PageNotFound.vue
+++ src/themes/vuetique/pages/PageNotFound.vue
@@ -2,29 +2,34 @@
   <div id="page_not_found">
     <section class="bg-grey-lighter py-12 px-12">
       <div class="container">
-        <div class="text-center mb-20">
+        <!-- <div class="text-center mb-20">
           <img src="/assets/icons/404.svg" alt="404">
-        </div>
+        </div> -->
+     
         <h2 class="text-center">
-          {{ $t("We can't find the page") }}
+          {{ $t("WHOOPS, OUR BAD...") }}
         </h2>
         <div class="text-center mt-10">
+          <h3 class="text-center">
+            {{ $t("The page you requested was not found, and we have a fine guess why.") }}
+          </h3>          
           <p>
-            {{ $t("Unfortunately we can't find the page you are looking for.") }}
+            {{ $t("If you typed the URL directly, please make sure the spelling is correct.") }}
           </p>
           <p>
-            {{ $t('If you need an assistance you can write to us through') }}
-            <router-link :to="localizedRoute('/contact')" class="cl-secondary no-underline">
-              {{ $t('a contact page') }}
-            </router-link>.
-          </p>
+            {{ $t("If you clicked on a link to get here, the link is outdated.") }}
+          </p>   
+          <h3 class="text-center">
+            {{ $t("What can you do?") }}
+          </h3>          
           <p>
-            {{ $t('You can also use') }}
-            <a href="#" class="cl-secondary no-underline" @click="toggleSearchpanel">
-              {{ $t('search') }}
-            </a>
-            {{ $t('to find product you were looking for.') }}
+            {{ $t("Have no fear, help is near! There are many ways you can get back on track with Magento Store.") }}
           </p>
+          <p>{{ $t("Go back to the previous page.") }}</p>  
+          <p>{{ $t("Use the search bar at the top of the page to search for your products.") }}</p>  
+          <p>{{ $t("Follow these links to get you back on track!") }}</p>  
+          
+          <p>{{"Store "}}<router-link :to="localizedRoute('/')" class="cl-secondary no-underline">{{"Home"}}</router-link>{{ $t(" | ") }}<router-link :to="localizedRoute('/my-account')" class="cl-secondary no-underline">{{"My Account"}}</router-link></p>  
         </div>
       </div>
     </section>
@@ -46,7 +51,7 @@
                       :key="product.id"
                       v-if="!product._dontShowInListingFlag"
                     >
-                      <product-tile
+                      <product-tile-carousel
                         class="collection-product"
                         :product="product"
                         :labels-active="false"
@@ -65,7 +70,7 @@
 
 <script>
 import PageNotFound from '@vue-storefront/core/pages/PageNotFound'
-import ProductTile from 'theme/components/core/ProductTile.vue'
+import ProductTileCarousel from 'theme/components/core/ProductTileCarousel'
 import { Carousel, Slide } from 'vue-carousel'
 import NoSSR from 'vue-no-ssr'
 
@@ -74,10 +79,13 @@ export default {
   data () {
     return {
       config: {
-        perPageCustom: [[576, 2], [1024, 4]],
-        paginationEnabled: true,
+        perPageCustom: [[0, 2], [768, 3], [1024, 4],[1440, 6],[1599, 5], [1600, 6], [1920, 7],[2560, 8]],
+        paginationEnabled: false,
         loop: false,
-        paginationSize: 6
+        paginationSize: 5,
+        navigationEnabled: true,
+        navigationNextLabel: `<button type="button" class="carousel-nav-nxt"><svg width="1em" height="1em" fill="currentColor" viewBox="0 0 24 22"><path d="M.75 11h22.5m-10.5 10.5L23.25 11 12.75.5" stroke="#0C1214" fill="none" stroke-linecap="round" stroke-linejoin="round"></path></svg></button>`,
+        navigationPrevLabel: `<button type="button" class="carousel-nav-pre"><svg width="1em" height="1em" fill="currentColor" viewBox="0 0 24 22"><path d="M23.25 11H.75M11.25.5L.75 11l10.5 10.5" stroke="#0C1214" fill="none" stroke-linecap="round" stroke-linejoin="round"></path></svg></button>`        
       }
     }
   },
@@ -89,7 +97,7 @@ export default {
   components: {
     Slide,
     Carousel,
-    ProductTile,
+    ProductTileCarousel,
     'no-ssr': NoSSR
   },
   methods: {
@@ -107,6 +115,69 @@ export default {
 
     &:hover {
       @apply text-primary;
+    }
+  }
+  .product {
+    &.collection-product {
+      padding: 0;
+    }
+  }
+  .collection-product {
+    .product-link {
+      @apply px-sm;
+    }
+
+    .product-image {
+      height: auto;
+      will-change: opacity;
+
+      img {
+        max-width: 100%;
+        max-height: 100%;
+        height: auto;
+        vertical-align: bottom;
+      }
+    }
+  }
+  .VueCarousel {
+    .VueCarousel-dot {
+      line-height: 1;
+
+      &:focus {
+        outline: none;
+      }
+    }
+
+    .VueCarousel-navigation--disabled {
+      display: none;
+    }
+
+
+    .carousel-nav-nxt { 
+      position: absolute;
+      left: -75px;
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      background: #ffffff;
+      bottom: 15px;
+      border: 1px solid #e2dddd;
+    }
+    .carousel-nav-nxt svg {
+      margin: 0 auto;
+    }
+    .carousel-nav-pre svg {
+      margin: 0 auto;
+    }
+    .carousel-nav-pre {
+      position: absolute;
+      right: -75px;
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      background: #ffffff;
+      bottom: 15px;
+      border: 1px solid #e2dddd;
     }
   }
 </style>
