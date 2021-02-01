14f3e4be19f6682e33232d3056b5f22a825147ae
rebinti <rebin.shaju@titechnologies.org>
2021-02-01 16:05:04 +0530

    added the accordion for the device screen footer

--- src/themes/vuetique/components/core/blocks/Footer/Footer.vue
+++ src/themes/vuetique/components/core/blocks/Footer/Footer.vue
@@ -4,12 +4,163 @@
               <Newsletter />
     </div> 
     <div
-      class="flex pt-8 pb-auto sm:pb-8 lg:pb-10 bg-grey-lighter mt-5"
+      class="flex pt-8 pb-auto sm:pb-8 lg:pb-10 bg-grey-lighter mt-5 footer-part"
       v-if="!isCheckoutPage"
     >
-      <div class="container">
-        <div class="row justify-center md:justify-between">
+      <div class="container footer-container device-only">
+        <div class="">
+          <button class="accordion"><h3>Help</h3></button>
+          <div class="panel">
+                    <div class="my-3" >
+                        <router-link :to="localizedRoute('/my-account')" class="menu-link footer-nav">
+                          {{ $t('My Account') }}
+                        </router-link>                           
+                    </div>
+                    <div class="my-3" >
+                        <router-link :to="localizedRoute('/help-faq')" class="menu-link footer-nav">
+                          {{ $t('FAQ’s') }}
+                        </router-link>                        
+                    </div>
+                    <div class="my-3" >
+                        <router-link :to="localizedRoute('/help-faq')" class="menu-link footer-nav">
+                          {{ $t('Help Centre') }}
+                        </router-link>                        
+                    </div>
+                    <div class="my-3" >
+                        <router-link :to="localizedRoute('/delivery-returns')" class="menu-link footer-nav">
+                          {{ $t('Delivery and Returns') }}
+                        </router-link>                         
+                    </div>
+                    <div class="my-3" >
+                        <!-- <a href="https://www.iclothing.com/privacy-centre/" aria-current="page" class="menu-link router-link-exact-active router-link-active">
+                            {{ $t('Privacy Centre') }}
+                        </a>                            -->                        
+                        <router-link :to="localizedRoute('/privacy-centre')" class="menu-link footer-nav">
+                          {{ $t('Privacy Centre') }}
+                        </router-link>                           
+                    </div>
+                    <div class="my-3" >
+                        <router-link :to="localizedRoute('/inspiration/pages/covid-19/')" class="menu-link footer-nav">
+                          {{ $t('COVID-19 Updates') }}
+                        </router-link>                         
+                    </div>             
+          </div>
 
+          <button class="accordion"><h3>Quick links</h3></button>
+          <div class="panel">
+                <div class="my-3" >
+                    <a href="/i/about-us" class="menu-link" >
+                        About Us
+                    </a>
+                </div>
+                <div class="my-3" >
+                    <a href="#" class="menu-link" >
+                        Partnerships
+                    </a>
+                </div>
+                <div class="my-3" >
+                    <a href="/gift-card" aria-current="page" class="menu-link router-link-exact-active router-link-active">
+                        Gift Card
+                    </a>
+                </div>
+                <div class="my-3" >
+                    <a href="/reviews" aria-current="page" class="menu-link router-link-exact-active router-link-active">
+                      Reviews
+                    </a>
+                </div>
+                <div class="my-3" >
+                    <a href="/students" aria-current="page" class="menu-link router-link-exact-active router-link-active">
+                      Students
+                    </a>
+                </div>
+          </div>
+
+          <button class="accordion"><h3> Join in on the chit chat </h3></button>
+          <div class="panel">
+            <div class="flex justify-start lg:justify-start social-footer mt-6 app-share-div">
+              <a
+                target="_blank"
+                rel="noopener"
+                class="social-icon mr-1 rounded-full new_footer_icons"
+                href="https://www.facebook.com/iclothingie"
+                :aria-label="$t('Go to Facebook')"
+              >
+                <!-- <img src="/assets/icons/social-f.svg" alt=""> -->
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a
+                target="_blank"
+                rel="noopener"
+                class="social-icon mx-1 rounded-full new_footer_icons"
+                href="https://www.instagram.com/iclothing"
+                :aria-label="$t('Go to Instagram')"
+              >
+                <!-- <img src="/assets/icons/social-i.svg" alt=""> -->
+                <i class="fab fa-instagram"></i>
+              </a>
+               <a
+                target="_blank"
+                rel="noopener"
+                class="social-icon mx-1 rounded-full new_footer_icons"
+                href="https://www.youtube.com/user/iclothingie"
+                :aria-label="$t('Go to Youtube')"
+              >
+                <!-- <img src="/assets/icons/social-y.svg" alt=""> -->
+                <i class="fab fa-youtube"></i>
+              </a>
+              <a
+                target="_blank"
+                rel="noopener"
+                class="social-icon mx-1 rounded-full new_footer_icons"
+                href="https://www.pinterest.com/iclothing"
+                :aria-label="$t('Go to Pinterest')"
+              >
+                <!-- <img src="/assets/icons/social-p.svg" alt=""> -->
+                <i class="fab fa-pinterest-p"></i>
+              </a> 
+              <a
+                target="_blank"
+                rel="noopener"
+                class="social-icon mx-1 rounded-full new_footer_icons"
+                href="https://twitter.com/iclothing"
+                :aria-label="$t('Go to Twitter')"
+              >
+                <i class="fab fa-twitter"></i>
+              </a>
+            </div>                        
+          </div>
+          <button class="accordion"><h3>Download our app</h3></button>
+          <div class="panel">
+
+            <div class="flex justify-start lg:justify-start social-footer mt-6 app-share-div">
+              <a
+                target="_blank"
+                rel="noopener"
+                class="social-icon mr-1 rounded-full new_footer_icons"
+                href="https://www.facebook.com/"
+                :aria-label="$t('Go to Facebook')"
+              >
+                <!-- <img src="/assets/icons/social-f.svg" alt=""> -->
+                <i class="fab fa-android"></i>
+              </a>
+
+               <a
+                target="_blank"
+                rel="noopener"
+                class="social-icon mr-1 rounded-full new_footer_icons"
+                href="https://www.facebook.com/"
+                :aria-label="$t('Go to Facebook')"
+              >
+                <!-- <img src="/assets/icons/social-f.svg" alt=""> -->
+                <i class="fab fa-apple"></i>
+              </a>
+    
+            </div>            
+          </div>          
+        </div>
+      </div>  
+      <div class="container footer-container web-only">        
+        <div class="row justify-center md:justify-between">
            <div class="footer-menu sm:flex col-12 md:col-6 lg:col-6 justify-around mt-8 lg:mt-0">
                 <!-- <cms-block :identifier="'footer_help_block_new'"  style="margin-top:-40px;"/> -->
                 <div>
@@ -397,6 +548,22 @@ export default {
       return (new Date()).getFullYear()
     }
   },
+  mounted () {
+      let acc = document.getElementsByClassName("accordion");
+      let i;
+
+      for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          let panel = this.nextElementSibling;
+          if (panel.style.display === "block") {
+            panel.style.display = "none";
+          } else {
+            panel.style.display = "block";
+          }
+        });
+      }
+  },  
   components: {
     Newsletter,
     LanguageSwitcher,
@@ -442,23 +609,6 @@ export default {
     -webkit-font-smoothing: antialiased;
   }
 }
-
-@media (max-width: 767px) {
-    .app-share-div {
-        -ms-flex-pack: start;
-        justify-content: start;
-        margin-top: 1rem;
-        margin-bottom: 1rem;
-    }
- }
-
- @media screen and (min-width: 728px) and (max-width: 1023px) {
-    .footer-app-dwld-div{
-          margin-top: 2rem;
-    }
-
-}
-
 .new_footer_icons {
       /* width: 25px; */
     /* height: 25px; */
@@ -486,7 +636,85 @@ export default {
     backface-visibility: hidden;
     border: 1px solid;
     padding-top: 1px;
+} 
+.accordion {
+  background-color: #eee;
+  color: #444;
+  cursor: pointer;
+  padding: 12px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+  transition: 0.4s;
+  border-bottom: 1px solid #d6d6d6;
 }
 
+.active, .accordion:hover {
+  background-color: #ccc; 
+}
+.panel {
+  padding: 0 18px;
+  display: none;
+  background-color: white;
+  overflow: hidden;
+}
+.device-only{
+  display:none;
+}
+.web-only{
+  display:block;
+}
+button.accordion:after {
+  position: absolute;
+  content: ">";
+  top: 0;
+  right: -36px;
+  width: 0;
+  height: 0;
+  display: inline-block;
+  border-top: 18px solid transparent;
+  border-bottom: 18px solid transparent;
+  border-left: 18px solid #2399e5;
+  border-right: 18px solid transparent;
+} 
+@media (max-width: 767px) {
+    .app-share-div {
+        -ms-flex-pack: start;
+        justify-content: start;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+    }
+ }
+
+ @media screen and (min-width: 728px) and (max-width: 1023px) {
+    .footer-app-dwld-div{
+          margin-top: 2rem;
+    }
+
+}
+@media (max-width: 576px) {
+  .device-only{
+    display:block;
+    padding-right: 0rem;
+    padding-left: 0rem;
+  }
+  .web-only{
+    display:none;
+  }
+  .footer-part{
+    padding-top: 0rem !important;
+    margin-top: 0rem !important;
+  }  
+}
+@media (max-width: 425px) {
+  footer .pb-8{    
+    padding-top: 0rem !important;
+  }
+  footer .pb-8 .container .row .mt-8{
+    padding-top: 0rem !important;
+  }
+}
 
 </style>
