3001909e6046ec58c90da75391ca16eac5c688f6
rebinti <rebin.shaju@titechnologies.org>
2021-02-04 19:34:16 +0530

    removed the swiper packages -from template -  for testing with js implimentation (2)

--- src/themes/vuetique/components/core/SwipersliderHome.vue
+++ src/themes/vuetique/components/core/SwipersliderHome.vue
@@ -1,5 +1,7 @@
 <template>
-  <swiper class="swiper" :options="swiperOptions">
+<div>
+  </div>
+  <!-- <swiper class="swiper" :options="swiperOptions">
     <swiper-slide  v-for="product in products"
           v-if="!product._dontShowInListingFlag"
           :key="product.id">
@@ -12,19 +14,19 @@
     <div class="swiper-pagination" slot="pagination"></div>
     <div class="swiper-button-prev" slot="button-prev"></div>
     <div class="swiper-button-next" slot="button-next"></div>
-  </swiper>
+  </swiper> -->
 </template>
 
 <script>
-  import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+  // import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
   import ProductTileCarousel from 'theme/components/core/ProductTileCarousel'
-  import 'swiper/swiper.scss';
+  // import 'swiper/swiper.scss';
   export default {
     name: 'swiper-example-loop-group',
     title: 'Loop mode with multiple slides per group',
     components: {
-      Swiper,
-      SwiperSlide,
+      // Swiper,
+      // SwiperSlide,
       ProductTileCarousel,
     },
     props: {
@@ -43,59 +45,59 @@
     },     
     data() {
       return {
-        swiperOptions: {
-          slidesPerView: 6,
-          spaceBetween: 2,
-          slidesPerGroup: 3,
-          lazy: true,
-          loop: false,
-          loopFillGroupWithBlank: false,
-          direction: 'horizontal',
-          freeMode: true,
-          pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-            progressbarOpposite: false,
-          },
-          navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-          },          
-          breakpoints: {
-            2560: {
-              slidesPerView: 8,
-              spaceBetween: 5
-            },              
-            1920: {
-              slidesPerView: 7,
-              spaceBetween: 5
-            },              
-            1600: {
-              slidesPerView: 6,
-              spaceBetween: 5
-            },              
-            1440: {
-              slidesPerView: 6,
-              spaceBetween: 5
-            },              
-            1024: {
-              slidesPerView: 4,
-              spaceBetween: 5
-            },
-            768: {
-              slidesPerView: 3,
-              spaceBetween: 5
-            },
-            640: {
-              slidesPerView: 2,
-              spaceBetween: 2
-            },
-            320: {
-              slidesPerView: 2,
-              spaceBetween: 2
-            }
-          }          
-        }
+        // swiperOptions: {
+        //   slidesPerView: 6,
+        //   spaceBetween: 2,
+        //   slidesPerGroup: 3,
+        //   lazy: true,
+        //   loop: false,
+        //   loopFillGroupWithBlank: false,
+        //   direction: 'horizontal',
+        //   freeMode: true,
+        //   pagination: {
+        //     el: '.swiper-pagination',
+        //     clickable: true,
+        //     progressbarOpposite: false,
+        //   },
+        //   navigation: {
+        //     nextEl: '.swiper-button-next',
+        //     prevEl: '.swiper-button-prev'
+        //   },          
+        //   breakpoints: {
+        //     2560: {
+        //       slidesPerView: 8,
+        //       spaceBetween: 5
+        //     },              
+        //     1920: {
+        //       slidesPerView: 7,
+        //       spaceBetween: 5
+        //     },              
+        //     1600: {
+        //       slidesPerView: 6,
+        //       spaceBetween: 5
+        //     },              
+        //     1440: {
+        //       slidesPerView: 6,
+        //       spaceBetween: 5
+        //     },              
+        //     1024: {
+        //       slidesPerView: 4,
+        //       spaceBetween: 5
+        //     },
+        //     768: {
+        //       slidesPerView: 3,
+        //       spaceBetween: 5
+        //     },
+        //     640: {
+        //       slidesPerView: 2,
+        //       spaceBetween: 2
+        //     },
+        //     320: {
+        //       slidesPerView: 2,
+        //       spaceBetween: 2
+        //     }
+        //   }          
+        // }
       }
     },  
   }
