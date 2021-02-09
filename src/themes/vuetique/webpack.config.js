c084795107226bf931202b165d11ce1dd7d94d67
rebinti <rebin.shaju@titechnologies.org>
2021-02-09 18:22:47 +0530

    webpack-bundle-analyzer enabled for theme specific

--- src/themes/vuetique/webpack.config.js
+++ src/themes/vuetique/webpack.config.js
@@ -1,4 +1,24 @@
 // You can extend default webpack build here. Read more on docs: https://github.com/DivanteLtd/vue-storefront/blob/master/doc/Working%20with%20webpack.md
-module.exports = function (config, { isClient, isDev }) {
-  return config
-}
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = function(config, { isClient, isDev }) {
+  let configLoaders;
+  if (isClient) {
+    configLoaders = config.module.rules;
+    config.plugins.push(
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+        statsFilename: 'test',
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      }),
+    );
+  } else {
+    configLoaders = config.module.rules;
+  }
+  configLoaders.push({
+    test: /\.json5$/,
+    loader: 'json5-loader',
+  });
+  return config;
+};
